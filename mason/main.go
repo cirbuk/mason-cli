8eeb470ee4a7a62bb6d13679420e3be8336fe58f
saurabmitra3 <saurab.mitra@kubric.io>
2022-05-19 11:55:58 +0530

    Removes older commands and updates in usage and help

--- mason/main.go
+++ mason/main.go
@@ -58,18 +58,18 @@ func main() {
 	pathProjectJson := projectCmd.String("path", "", "Relative file path of JSON for project create/update")
 	outputProject := projectCmd.String("out", "", "Relative path to write output to")
 
-	schemaCmd := flag.NewFlagSet("schema", flag.ExitOnError)
-	actionSchema := schemaCmd.String("action", "", "Please  enter action from [create,update,delete,get]")
-	allSchema := schemaCmd.Bool("all", false, "Get all schemas")
-	idSchema := schemaCmd.String("id", "", "Mason Schema ID")
-	pathSchemaJson := schemaCmd.String("path", "", "Relative file path of JSON for schema create/update")
-	outputSchema := schemaCmd.String("out", "", "Relative path to write output to")
-
-	contentCmd := flag.NewFlagSet("content", flag.ExitOnError)
-	actionContent := contentCmd.String("action", "", "Please  enter action from [get]")
-	allContent := contentCmd.Bool("all", false, "Get all content")
-	idContent := contentCmd.String("id", "", "Mason Content ID")
-	outputContent := contentCmd.String("out", "", "Relative path to write output to")
+	// 	schemaCmd := flag.NewFlagSet("schema", flag.ExitOnError)
+	// 	actionSchema := schemaCmd.String("action", "", "Please  enter action from [create,update,delete,get]")
+	// 	allSchema := schemaCmd.Bool("all", false, "Get all schemas")
+	// 	idSchema := schemaCmd.String("id", "", "Mason Schema ID")
+	// 	pathSchemaJson := schemaCmd.String("path", "", "Relative file path of JSON for schema create/update")
+	// 	outputSchema := schemaCmd.String("out", "", "Relative path to write output to")
+	//
+	// 	contentCmd := flag.NewFlagSet("content", flag.ExitOnError)
+	// 	actionContent := contentCmd.String("action", "", "Please  enter action from [get]")
+	// 	allContent := contentCmd.Bool("all", false, "Get all content")
+	// 	idContent := contentCmd.String("id", "", "Mason Content ID")
+	// 	outputContent := contentCmd.String("out", "", "Relative path to write output to")
 
 	if len(os.Args) < 2 {
 		handleHelp(helpCmd)
@@ -91,10 +91,10 @@ func main() {
 		handleLogout(logoutCmd)
 	case "project": // if its the 'project' command
 		handleProject(projectCmd, actionProject, allProject, idProject, pathProjectJson, outputProject)
-	case "schema": // if its the 'schema' command
-		handleSchema(schemaCmd, actionSchema, allSchema, idSchema, pathSchemaJson, outputSchema)
-	case "content": // if its the 'content' command
-		handleContent(contentCmd, actionContent, allContent, idContent, outputContent)
+		// 	case "schema": // if its the 'schema' command
+		// 		handleSchema(schemaCmd, actionSchema, allSchema, idSchema, pathSchemaJson, outputSchema)
+		// 	case "content": // if its the 'content' command
+		// 		handleContent(contentCmd, actionContent, allContent, idContent, outputContent)
 	default: // if we don't understand the input
 		println("error. See mason help")
 	}
@@ -115,8 +115,9 @@ func ValidCommand(cmd string) bool {
 	switch cmd {
 	case
 		"project",
-		"content",
-		"schema":
+		"login",
+		"logout",
+		"init":
 		return true
 	}
 	return false
